ddb53b617f7a5b3b16d5bb1f4ba396a0e5100783
Joseph Hickey <joseph.p.hickey@nasa.gov>
2022-11-14 10:35:14 -0500

    Add Wait For TLM

--- scripts/handler.lua
+++ scripts/handler.lua
@@ -1,5 +1,13 @@
 print "handler.lua is being loaded!"
 
+mypipe = CFE.CreatePipe(5, "LUAPIPE")
+print ("Pipe ID is: " .. tostring(mypipe))
+
+inintf = EdsDB.GetInterface("CFE_ES/Application/HK_TLM")
+assert(inintf)
+outintf = EdsDB.GetInterface("CFE_ES/Application/CMD")
+assert(outintf)
+
 function TestMessageHandler(inmsg)
     print "This is TestMessageHandler()"
 
@@ -10,8 +18,12 @@ function TestMessageHandler(inmsg)
         print ("The Input value is: " .. inmsg.Value())
     end
 
-    intf = EdsDB.GetInterface("CFE_ES/Application/CMD")
-    outmsg = EdsDB.NewMessage(intf, "NoopCMD")
+    intlm = CFE.WaitFor(inintf, mypipe, 5000)
+    if (intlm) then
+        print ("Got a TLM: " .. tostring(intlm))
+        print ("CommandCounter: " .. tostring(intlm.Payload.CommandCounter()))
+    end
 
+    outmsg = EdsDB.NewMessage(outintf, "NoopCMD")
     CFE.SendMsg(outmsg)
 end
